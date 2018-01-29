1ba5e6110f176abc2cea0a570f9510056abe3589
Hiten Sethiya <hitensethiya@hotmail.com>
2018-01-29 21:33:41 +0530

    Added packets count

--- ping.py
+++ ping.py
@@ -8,15 +8,15 @@ import time
 url = "http://www.spfld.com/cgi-bin/ping"
 trace_url = "https://api.hackertarget.com/mtr/"
 
-hosts = ["google.com"##addhostshere
-        ]
+hosts = ["google.com"]
 sizes = ["64", "128", "256", "512", "1024", "2048"]
 for host in hosts:
     query = {'q': host}
     response = requests.request("GET", trace_url, params=query)
     with open('ping_traceroutes.txt', 'a') as tr_record:
-        tr_record.write(host + " Local Time :: " + str(time.strftime("%d %b %Y %H:%M:%S" , time.localtime())) + "\n")
+        tr_record.write("----------------------------------\n"+host + " Local Time :: " + str(time.strftime("%d %b %Y %H:%M:%S", time.localtime())) + "\n")
         tr_record.write(response.text)
+    print("printed traceroot")
     for size in sizes:
         querystring = {"remote_host": host, "dns": "on", "count": "20", "size": size}
         response = requests.request("GET", url, params=querystring)
@@ -30,14 +30,18 @@ for host in hosts:
             continue
         print(host, size, 'Done', 'response = ', response.text.splitlines()[i])
         with open('ping_results.csv', 'a') as csvfile:
-            fieldnames = ['Host', 'Frame Size', 'RTT Min', 'RTT Avg', 'RTT Max', 'RTT Mdev', 'Time']
+            fieldnames = ['Host', 'Frame Size','Packets Recieved','Packets Transmitted','Packet loss','Total Time', 'RTT Min', 'RTT Avg', 'RTT Max', 'RTT Mdev', 'Time']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writerow({'Host': host,
                              'Frame Size': size,
-                             'RTT Min': response.text.splitlines()[i].split('/')[3].split('=')[1].strip(),
-                             'RTT Avg': response.text.splitlines()[i].split('/')[4],
-                             'RTT Max': response.text.splitlines()[i].split('/')[5],
-                             'RTT Mdev': response.text.splitlines()[i].split('/')[6].split()[0].strip(),
+                             'Packets Recieved':response.text.splitlines()[i-1].split()[3].strip(),
+                             'Packets Transmitted':response.text.splitlines()[i-1].split()[0].strip(),
+                             'Packet loss':response.text.splitlines()[i-1].split()[5].strip(),
+                             'Total Time':response.text.splitlines()[i-1].split()[9].strip(),
+                             'RTT Min': response.text.splitlines()[i].split('/')[3].split('=')[1].strip()+'ms',
+                             'RTT Avg': response.text.splitlines()[i].split('/')[4]+'ms',
+                             'RTT Max': response.text.splitlines()[i].split('/')[5]+'ms',
+                             'RTT Mdev': response.text.splitlines()[i].split('/')[6].split()[0].strip()+'ms',
                              'Time': time.strftime("%d %b %Y %H:%M:%S", time.localtime())
                              })
 print('Finished! Check the results in \'ping_results.csv\' and \'ping_traceroutes\' Enjoy!!')
